165c855591d301f65c6f786d7e53ffdf6ab26430
TheUnknownL <prajwalmaharjan392@gmail.com>
2025-05-11 09:14:13 +0545

    fix bugs and tried to optimize

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 /*
-    KNOWN BUGS:
-            2: when there is duplication when printing out the line if there are multiple search words present
+    KNOWN BUGS/annoyance:
+            2: (fixed) when there is duplication when printing out the line if there are multiple search words present
+            3: don't like how I have to free.
             
 */
 
@@ -8,13 +9,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "./queue/queue.h"
-#include "./dynamic array/da.h"
+#include <ctype.h>
+#include "queue//queue.h"
 
 #define BUFF 1024
+#define ESCAPE " \r\n,.[]{}():;'"
 void printQueue(struct que *queue, FILE * fp);
+char * stringLower(const char *str);
 
 int main(const int args, const char* argv[]){
+
+    if(args<3){
+        printf("Incorrect argument\n./main <file> <argument(s)>\n\tEG: ./main ./yo/hi.txt test 1\n");
+        exit(1);
+    }
 
     struct que queue; 
     innitQueue(&queue);
@@ -22,31 +30,40 @@ int main(const int args, const char* argv[]){
     FILE * fp = fopen(argv[1],"rb");
     if(fp==NULL){
         printf("Couldn't open file or argument incorrect. ./main <file> <argument(s)>\n\tEG: ./main ./yo/hi.txt test 1");
-        fclose(fp);
         exit(1);
     }
 
-
+    
     char *line=(char *)malloc(sizeof(char)*BUFF);
     char *temp;
-
     
-    for(int i=2;i<args;i++){
-        while(fgets(line,BUFF,fp)!=NULL){ // takes the whole line
-        temp=strtok(line," \r\n");
-            while(temp!=NULL){ // takes a word from a the line
-                if(strcmp(argv[i],temp)==0){
+    while(fgets(line,BUFF,fp)!=NULL){ // takes the whole line
+        int inserted = 0;
+        temp=strtok(line,ESCAPE);
+        while(temp!=NULL){ // takes a word from a the line
+            for(int i=2;i<args;i++){
+                char *freeA=stringLower(argv[i]);
+                char *freeB=stringLower(temp);
+                if(strcmp(freeA,freeB)==0){
+                    free(freeA);
+                    free(freeB);
                     if(isfull(&queue)){
                         printQueue(&queue,fp);
                     }
                     enqueue(&queue,ftell(fp)-2); // fgets scans until the cursor points to first character of another line so we go back 2 to go back to first line. if it was -1, cursor would point to new line.
+                    inserted = 1;
                     break;
                 }
-                temp=strtok(NULL," \r\n"); // ignores the carriage return and new line when strtok gets those too by reading the string before new line.
+                free(freeA);
+                free(freeB);
             }
+            if(inserted){
+                break;            
+            }
+            temp=strtok(NULL,ESCAPE); // ignores the carriage return and new line when strtok gets those too by reading the string before new line.
         }
-        rewind(fp);
     }
+
     printQueue(&queue,fp);
     fclose(fp);
     free(line);
@@ -70,4 +87,16 @@ void printQueue(struct que *queue, FILE * fp){
         printf("%s\n",line);
     }
     free(line);
+}
+
+char * stringLower(const char *ch){
+    int i=0;
+    char *str=(char *)malloc(strlen(ch)+1);
+    if(str==NULL) exit(1);
+    strcpy(str,ch);
+    while(str[i]!=(char)'\0'){
+        str[i]=tolower(str[i]);
+        i++;
+    }
+    return str;
 }
