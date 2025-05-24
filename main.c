baeca78b0a2a85802776ee228d1aeb4825b5e325
TheUnknownL <prajwalmaharjan392@gmail.com>
2025-05-24 23:38:08 +0545

    corrected mistakes

--- main.c
+++ main.c
@@ -14,7 +14,7 @@
 
 #define BUFF 1024
 #define ESCAPE " \r\n,.[]{}():;'"
-void printQueue(struct que *queue, FILE * fp);
+void printQueue(struct que *queue,struct que *lineNumbers, FILE * fp);
 char * stringLower(const char *str);
 
 int main(const int args, const char* argv[]){
@@ -27,6 +27,9 @@ int main(const int args, const char* argv[]){
     struct que queue; 
     innitQueue(&queue);
 
+    struct que lineNumbers; 
+    innitQueue(&lineNumbers);
+
     FILE * fp = fopen(argv[1],"rb");
     if(fp==NULL){
         printf("Couldn't open file or argument incorrect. ./main <file> <argument(s)>\n\tEG: ./main ./yo/hi.txt test 1");
@@ -36,8 +39,9 @@ int main(const int args, const char* argv[]){
     
     char *line=(char *)malloc(sizeof(char)*BUFF);
     char *temp;
-    
+    size_t lineNo=0;
     while(fgets(line,BUFF,fp)!=NULL){ // takes the whole line
+        ++lineNo;
         int inserted = 0;
         temp=strtok(line,ESCAPE);
         while(temp!=NULL){ // takes a word from a the line
@@ -48,9 +52,10 @@ int main(const int args, const char* argv[]){
                     free(freeA);
                     free(freeB);
                     if(isfull(&queue)){
-                        printQueue(&queue,fp);
+                        printQueue(&queue,&lineNumbers,fp);
                     }
                     enqueue(&queue,ftell(fp)-2); // fgets scans until the cursor points to first character of another line so we go back 2 to go back to first line. if it was -1, cursor would point to new line.
+                    enqueue(&lineNumbers,lineNo);
                     inserted = 1;
                     break;
                 }
@@ -64,15 +69,16 @@ int main(const int args, const char* argv[]){
         }
     }
 
-    printQueue(&queue,fp);
+    printQueue(&queue,&lineNumbers,fp);
     fclose(fp);
     free(line);
     return 0;
 }
 
-void printQueue(struct que *queue, FILE * fp){
+void printQueue(struct que *queue,struct que * lineNumbers, FILE * fp){
     char *line=(char *)malloc(sizeof(char)*BUFF);
     char ch;
+    long save = ftell(fp);
     while(!isempty(queue)){
         fseek(fp,dequeue(queue),SEEK_SET);
         while((ch=fgetc(fp))!='\n'){ // moves cursur backward until it encounters new line
@@ -84,8 +90,9 @@ void printQueue(struct que *queue, FILE * fp){
             }
         }
         fgets(line,BUFF,fp);
-        printf("%s\n",line);
+        printf("%zu:\t%s",dequeue(lineNumbers),line);
     }
+    fseek(fp,save,SEEK_SET);
     free(line);
 }
 
